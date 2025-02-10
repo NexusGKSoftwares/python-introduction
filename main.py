b89b174c11babcf9da739b066c2afbf400ea9fba
NexusGKSoftwares <nexusgk3@gmail.com>
2025-02-10 21:49:30 +0300

    new

---
+++ main.py
@@ -0,0 +1,140 @@
+
+# what is python?
+#  high intergrated programming language
+#  it is widely used in many fields such as web development, data analysis, machine learning, automation, etc.
+
+#  it is easy to learn and use, and it has a large and active community of developers
+
+# key features of python:
+# 1. easy to learn and use
+# 2. high level language
+# 3. interpreted language
+# 4. object oriented
+# 5 open source and community driven
+# 6. cross platform
+
+# installation of python
+# 1. download the python installer from the official python website
+# 2. follow the installation instructions to install python on your computer
+
+# test the installation of python
+# python --version
+
+# running python code in vs code
+# 1. open vs code
+# 2. create a new file or open an existing file
+# 3. write python code in the file
+# 4. save the file with a .py extension
+# 5. click on the run button or press f5 to run the code
+# 6. the output will be displayed in the output panel
+# 7. you can also use the terminal to run python code by typing python and then file name 
+# 8. you can also use the debugger to debug your code
+
+# first topic
+
+# python syntax,variables and data types
+# 1. python syntax
+# deals with indentation
+
+# example of python syntax code 
+# print("hello world") # this is a print statement(output is - hello world)
+
+# print() - is a function in python that is used to print output to the screen
+
+# 2.variables and data types
+# variables are used to store data in python
+# data types are used to define the type of data that a variable can hold
+
+# example
+# name = "john" 
+# age = 25 
+# df = "gideon"
+# print(name) # output is - john
+# print(age) # output is - 25
+# print(df)
+
+# data types in python
+# 1. integer 
+# 2. string
+# 3. float
+# 4. boolean
+# 5. list
+# 6. tuple
+# 7. dictionary
+# 8. set
+# 9. complex
+# 10. bytes
+
+# 1. integer
+#  it is a whole number, positive or negative, without a fractional part.
+# example
+# age = 25
+# print(age) # output is - 25
+
+# 2. string
+# it is a sequence of characters, such as a word or phrase, enclosed in quotes.
+# example
+# name = "john"
+# print(name) # output is - john
+
+# 3. float
+# it is a real number, positive or negative, with a fractional part.decimal number
+# example
+# height = 5.5
+# print(height) # output is - 5.5
+
+# 4. boolean
+# it is a logical value that can be either true or false.
+# example
+# is_adult = True
+# print(is_adult) # output is - True
+
+# is_male = False
+# print(is_male) # output is - False
+
+
+# comments
+# comments are used to add notes to your code, they are ignored by the interpreter.
+# comments makes the code understable
+# there are two types of comments in python
+# 1. single line comment 
+#  it is denoted by # symbol
+# example
+# # this is a comment
+# print("hello world") # this is a comment and a print statement
+# 2. multi line comment
+# it is denoted by """ """ or ''' ''' symbol
+# example
+
+'''
+what is python?
+python is a high level language
+it is easy to learn and use
+
+'''
+
+# input/output 
+# input: it is used to get input from the user
+# output: it is used to display output to the user
+
+# example
+# name = input("what is your name? ")
+# print("hello " + name) # output is - hello your name
+# output is - hello your name
+
+# example of a password
+password = input("enter your password: ")
+print("your password is: " + password) # output is - your password is: your password
+# output is - your password is: your password
+
+
+# summary
+#  python syntax
+#  python variables
+#  python data types
+#  python comments
+#  python input/output
+
+
+
+
