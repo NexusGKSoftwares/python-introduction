cac049adc3661de4ed55e38a497071704ee0a8d3
NexusGKSoftwares <nexusgk3@gmail.com>
2025-02-15 14:11:02 +0300

    Added header comments to week 2/day1.py.

---
+++ week 2/day1.py
@@ -0,0 +1,5 @@
+# week 2 of our 4-week course on the basics of python programming
+# author: Gideon
+# date: 2025-02-17
+# version: 1.0
+# description: This script will demonstrate the use of lists in python programming.
